31d19083443ea6ddd55950042e6bd9ba2434ecdd
MauriVaz <mvazquezcostal@gmail.com>
2023-02-02 15:14:11 -0600

    melgmle

--- public/index.js
+++ public/index.js
@@ -3,6 +3,7 @@ hour = () => {
   hora = fecha.getHours();
   minuto = fecha.getMinutes();
   segundo = fecha.getSeconds();
+  huso=Number(GMT=fecha.toString().substring(28,fecha.toString().length-26))
   if (hora < 10) {
     hora = '0' + hora;
   }
@@ -12,18 +13,20 @@ hour = () => {
   if (segundo < 10) {
     segundo = '0' + segundo;
   }
-  mireloj = hora + ' : ' + minuto + ' : ' + segundo;
+  
+  mireloj ="GMT "+ huso+ "<br/> " +hora + ' : ' + minuto + ' : ' + segundo;
   return mireloj;
 };
 Año = () => {
   x = new Date();
+  diaSemana=x.toString().substring(0,3)
   dia = x.getDate();
   mes = x.getMonth() + 1;
   if (mes < 10) {
     mes = '0' + mes;
   }
   año = x.getUTCFullYear();
-  miFecha = dia + '/' + mes + '/' + año;
+  miFecha = diaSemana + " " +dia + '/' + mes + '/' + año;
   return miFecha;
 };
 actualizarHora = () => {
@@ -37,6 +40,12 @@ actualizarFecha = () => {
   miFecha = document.getElementById('fecha');
   miFecha.innerHTML = mifecha;
 };
+
+actualizarTitle=()=>{
+  miHola=document.getElementById("titleChange")
+  miHola.innerHTML = "Show the date and hour"
+}
+setInterval(actualizarTitle,1000)
 setInterval(actualizarHora, 1000);
 setInterval(actualizarFecha, 1000);
 
